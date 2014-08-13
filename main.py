cf58000f745ca5b0851366b15edabfa29de131b5
Maxiee <maxieewong@gmail.com>
2014-08-14 00:45:51 +0800

    add a simple status bar. database init. new_button pressed fuction.

--- main.py
+++ main.py
@@ -2,20 +2,12 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import bookmodel
 import database
+import dlgnew
 import time
 
 class Form(QWidget):
     def __init__(self, parent = None):
         super(Form, self).__init__(parent)
-
-        # Test: Suppose we have 3 books
-        books = [
-                bookmodel.Book("Book 1", time.time(), 0, 350, 0, False),
-                bookmodel.Book("Book 2", time.time(), 0, 400, 0, False),
-                bookmodel.Book("Book 3", time.time(), 0, 450, 0, False)]
-        # instantiate database
-        my_db = database.Database()
-        my_db.init()
 
         # UI Widget
         self.presentation_combo = QComboBox()
@@ -26,10 +18,7 @@ class Form(QWidget):
         self.new_button.clicked.connect(self.new_pressed)
 
         self.books_list = QListWidget()
-        for book in books:
-            self.books_list.addItem(book.title)
-        self.books_list.setFixedHeight(50)
-
+        
         self.title_label = QLabel("Title:")
         self.add_date_label = QLabel("Add date:")
         self.page_count_label = QLabel("Page count:")
@@ -39,6 +28,8 @@ class Form(QWidget):
         self.giveup_button = QPushButton("Give Up")
 
         self.finish_button = QPushButton("Finish")
+
+        self.status_info = QLabel()
 
         # UI Layout
         main_layout = QVBoxLayout()
@@ -68,6 +59,7 @@ class Form(QWidget):
         main_layout.addWidget(self.books_list)
         main_layout.addWidget(top_two_group)
         main_layout.addLayout(top_three)
+        main_layout.addWidget(self.status_info)
 
         # Set Layout
         self.setLayout(main_layout)
@@ -75,8 +67,37 @@ class Form(QWidget):
         # Set Title
         self.setWindowTitle("Reading Track")
 
+        # Test: Suppose we have 3 books
+        books = [
+                bookmodel.Book("Book 1", time.time(), 0, 350, 0, False),
+                bookmodel.Book("Book 2", time.time(), 0, 400, 0, False),
+                bookmodel.Book("Book 3", time.time(), 0, 450, 0, False)]
+
+        # init book list
+        for book in books:
+            self.books_list.addItem(book.title)
+        self.books_list.setFixedHeight(50)
+
+        # instantiate database
+        self.my_db = database.Database()
+        self.my_db.init()
+
+        # UI init
+        self.init_status()
+
+    def init_status(self):
+        self.status_info.setText("Now you have " + 
+                str(self.my_db.get_count()) + 
+                " books on reading...")
+
     def new_pressed(self):
-        pass
+        new_dlg = dlgnew.NewDialog()
+        if new_dlg.exec_() == QDialog.Accepted:
+            print("New dialog OK!")
+            print("New title is:" + new_dlg.title)
+            print("It has :" + new_dlg.pages)
+        else:
+            print("New dialog Cancel!")
 
 if __name__ == '__main__':
     import sys
